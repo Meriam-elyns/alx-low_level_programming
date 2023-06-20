4890d36dbdc1f571d330c5e4f3e7ecf45ff2842d
Meriam-elyns <meriamelyns@gmail.com>
2023-06-20 12:59:21 -0700

    zero

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,9 +34,9 @@ printf(", %lu", aft1 + (aft2 / l));
 printf("%lu", aft2 % l);
 aft1 = aft1 + bef1;
 bef1 = aft1 - bef1;
-aft1 = aft2 + bef2;
+aft2 = aft2 + bef2;
 bef2 = aft2 - bef2;
 }
 printf("\n");
-return(0);
+return (0);
 }
